c915d8b71760074618cbabbc3a099b028b6b442f
Ivan Kondratev <ivanxab54@gmail.com>
2025-09-27 17:25:09 +0300

    chore: add sass in vite

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { router } from "@/router.ts";
 import App from "./App.vue";
+import '@/assets/styles/index.scss';
 
 createApp(App)
   .use(createPinia())
--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,12 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/assets/styles/vars" as *;`,
+      },
+    },
+  },
   plugins: [vue()],
 });
