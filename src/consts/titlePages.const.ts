f9fa10fcb67faf9c4da8a7a28a1ecaf9e2f80d31
AndiMandi <Andre200344@bk.ru>
2025-10-08 21:09:20 +0700

    Добавлено: наименование вкладки приложения по страницам

---
+++ src/consts/titlePages.const.ts
@@ -0,0 +1,6 @@
+export const TitlePage = {
+    MAIN_LAYOUT: "Default page",
+    LOGIN_PAGE: "Авторизация",
+    PUBLIC_PAGE: "Публичная страница",
+    PROTECTED_PAGE: "Защищенная страница"
+} as const;
--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,6 @@
 import { createWebHistory, createRouter } from "vue-router";
 import { RouteName } from "@/consts/routeNames.const.ts";
+import { TitlePage } from "@/consts/titlePages.const.ts";
 
 const MainPage = () => import("@/layouts/BaseLayout.vue");
 const PublicPage = () => import("@/pages/PublicPage.vue");
@@ -16,17 +17,20 @@ const routes = [
       {
         path: "/login",
         name: RouteName.LOGIN_PAGE,
-        component: LoginPage
+        component: LoginPage,
+        meta: {title: TitlePage.LOGIN_PAGE}
       },
       {
         path: "/public",
         name: RouteName.PUBLIC_PAGE,
-        component: PublicPage
+        component: PublicPage,
+        meta: {title: TitlePage.PUBLIC_PAGE}
       },
       {
         path: "/protected",
         name: RouteName.PROTECTED_PAGE,
-        component: ProtectedPage
+        component: ProtectedPage,
+        meta: {title: TitlePage.PROTECTED_PAGE}
       }
     ]
   }
@@ -35,4 +39,9 @@ const routes = [
 export const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, _, next) => {
+  document.title =  to.meta.title as string;
+  next();
 });
