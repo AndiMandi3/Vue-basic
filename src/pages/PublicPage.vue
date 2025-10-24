4c128441d08c302d2cfc0c1da3dcd99d4a995665
Димаков Андрей Владиславович <Dimakov_AV@sever.local>
2025-10-24 10:27:06 +0700

    refactored: Изменение подхода верстки компонента

--- src/pages/PublicPage.vue
+++ src/pages/PublicPage.vue
@@ -1,7 +1,37 @@
 <script setup lang="ts">
+import { computed } from "vue";
 import MainInfo from "@/pages/publicPage/components/MainInfo.vue";
 import LineDivider from "./publicPage/components/LineDivider.vue";
-import ContactInfo from "@/pages/publicPage/components/ContactInfo.vue";
+import CalendarIcon from "@/assets/images/calendar-icon.svg?component";
+import EmailIcon from "@/assets/images/email-icon.svg?component";
+import LocationIcon from "@/assets/images/location-icon.svg?component";
+import PhoneIcon from "@/assets/images/phone-icon.svg?component";
+import TelegramIcon from "@/assets/images/telegram-icon.svg?component";
+import ContactInfo from "./publicPage/components/ContactInfo.vue";
+
+const data = computed(() => [
+  {
+    value: "04.04.2003",
+    component: CalendarIcon,
+  },
+  {
+    value: "Andre200344@bk.ru",
+    component: EmailIcon,
+  },
+  {
+    value: "Г. Новосибирск",
+    component: LocationIcon,
+  },
+  {
+    value: "+7 (999)-464-85-91",
+    component: PhoneIcon,
+    isPriority: true,
+  },
+  {
+    value: "https://t.me/andimandi03",
+    component: TelegramIcon,
+  },
+]);
 </script>
 
 <template>
@@ -12,14 +42,21 @@ import ContactInfo from "@/pages/publicPage/components/ContactInfo.vue";
 
       <LineDivider />
 
-      <ContactInfo
-          email="Andre200344@bk.ru"
-          date-birth="04.04.2003"
-          city="Новосибирск"
-          phone="+7 (999)-464-85-91"
-          telegramLink="https://t.me/andimandi03"
-          priority-contact="phone"
-      />
+      <div class="contact-info">
+        <h2 class="contact-info__title">Личная информация пользователя</h2>
+
+        <div class="contact-info__elements">
+          <ContactInfo v-for="(item, index) in data" :key="index" class="contact-info__item">
+            <template #prepand>
+              <component :is="item.component" class="contact-info__item-icon" />
+              <p class="contact-info__item-text">{{ item.value }}</p>
+            </template>
+            <template v-if="item.isPriority" #append>
+              <span class="contact-info__item-note"> — предпочитаемый способ связи</span>
+            </template>
+          </ContactInfo>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -41,6 +78,41 @@ import ContactInfo from "@/pages/publicPage/components/ContactInfo.vue";
     border: 1px solid $gray-color;
     padding: 20px 50px;
     border-radius: 30px;
+  }
+}
+
+.contact-info {
+  padding: 20px 0;
+
+  &__title {
+    text-align: center;
+  }
+
+  &__elements {
+    padding-top: 30px;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+
+    &-empty {
+      padding: 20px 0;
+      display: flex;
+      justify-content: center;
+
+      &-text {
+        font-weight: $bold-font;
+      }
+    }
+  }
+
+  &__item {
+    display: flex;
+    flex: 1 1 49%;
+    gap: 10px;
+
+    &-note {
+      font-weight: $bold-font;
+    }
   }
 }
 </style>
--- src/pages/publicPage/components/ContactInfo.vue
+++ src/pages/publicPage/components/ContactInfo.vue
@@ -1,125 +1,15 @@
 <script setup lang="ts">
-import { computed } from "vue";
-import CalendarIcon from "@/assets/images/calendar-icon.svg?component";
-import EmailIcon from "@/assets/images/email-icon.svg?component";
-import LocationIcon from "@/assets/images/location-icon.svg?component";
-import PhoneIcon from "@/assets/images/phone-icon.svg?component";
-import TelegramIcon from "@/assets/images/telegram-icon.svg?component";
-
-interface IProps {
-  dateBirth?: string,
-  email?: string,
-  city?: string,
-  phone?: string,
-  telegramLink?: string,
-  priorityContact?: "email" | "telegram" | "phone",
+interface ISlots {
+  prepand: string,
+  append: string,
 }
-const props = defineProps<IProps>();
 
-const datas = computed(() => [
-  {
-    value: props.dateBirth,
-    component: CalendarIcon,
-    title: "Дата рождения",
-    hasValue: !!props.dateBirth,
-  },
-  {
-    value: props.email,
-    component: EmailIcon,
-    title: "Адрес электронной почты",
-    hasValue: !!props.email,
-    isPriority: props.priorityContact === "email",
-  },
-  {
-    value: props.city ? "г. " + props.city : undefined,
-    component: LocationIcon,
-    title: "Местонахождение",
-    hasValue: !!props.city,
-  },
-  {
-    value: props.phone,
-    component: PhoneIcon,
-    title: "Контактный номер телефона",
-    hasValue: !!props.phone,
-    isPriority: props.priorityContact === "phone",
-  },
-  {
-    value: props.telegramLink,
-    component: TelegramIcon,
-    title: "Ссылка на Telegram-аккаунт",
-    hasValue: !!props.telegramLink,
-    isPriority: props.priorityContact === "telegram",
-  },
-].filter(item => item.hasValue));
-
-const hasAnyData = computed(() => {
-  return !!(props.dateBirth || props.email || props.city || props.phone || props.telegramLink);
-});
-
-const isPriorityClass = computed(() => ({"contact-info__item-note": props.priorityContact}))
+defineSlots<ISlots>();
 </script>
 
 <template>
-  <div class="contact-info">
-    <h2 class="contact-info__title">Личная информация пользователя</h2>
-
-    <div v-if="hasAnyData" class="contact-info__elements">
-      <div
-          v-for="item in datas"
-          :key="item.title"
-          class="contact-info__item"
-      >
-        <component
-            :is="item.component"
-            :title="item.title"
-            class="contact-info__item-icon"
-        />
-        <p class="contact-info__item-text">{{ item.value || 'Не указано' }}</p>
-        <span
-            v-if="item.isPriority"
-            :class="isPriorityClass"
-        > — предпочитаемый способ связи</span>
-      </div>
-    </div>
-    <div v-else class="contact-info__elements-empty">
-      <p class="contact-info__elements-empty-text">Пользователь не указал данных</p>
-    </div>
+  <div>
+    <slot name="prepand" v-if="$slots.prepand" />
+    <slot name="append" v-if="$slots.append" />
   </div>
 </template>
-
-<style scoped lang="scss">
-.contact-info {
-  padding: 20px 0;
-
-  &__title {
-    text-align: center;
-  }
-
-  &__elements {
-    padding-top: 30px;
-    display: flex;
-    flex-wrap: wrap;
-    gap: 20px;
-
-    &-empty {
-      padding: 20px 0;
-      display: flex;
-      justify-content: center;
-
-      &-text {
-        font-weight: $bold-font;
-      }
-    }
-  }
-
-  &__item {
-    display: flex;
-    flex: 1 1 49%;
-    gap: 10px;
-
-    &-note {
-      font-weight: $bold-font;
-    }
-  }
-}
-</style>
